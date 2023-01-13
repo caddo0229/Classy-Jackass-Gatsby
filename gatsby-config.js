b73b8c565d5c43b79d338a8275bcf656a871e858
unknown <oscarsolano1028@gmail.com>
2023-01-13 09:36:01 -0500

    mux video swap error fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,32 +33,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Landify UI`,
-        short_name: `Landify UI`,
-        start_url: `/`,
-        background_color: `#7C3AED`,
-        theme_color: `#7C3AED`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // Replace with your favicon (This path is relative to the root of the site)
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
@@ -73,7 +47,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
-        endpoint: "https://gmail.us9.list-manage.com/subscribe/post?u=9135beb95db96a97303339685&amp;id=09762866e1&amp;f_id=00b10be1f0", // string; add your MC list endpoint here; see instructions below
+        endpoint:
+          "https://gmail.us9.list-manage.com/subscribe/post?u=9135beb95db96a97303339685&amp;id=09762866e1&amp;f_id=00b10be1f0", // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,6 @@ import About from "../components/about";
 import Metrics from "../components/metrics";
 import Signup from "../components/signup";
 import Team from "../components/team";
-// import Feed from "../components/feed";
-// import Faq from "../components/faq";
 
 const IndexPage = () => {
   return (
@@ -19,14 +17,6 @@ const IndexPage = () => {
       <Metrics />
       <Team />
       <Signup />
-      {/* <div className="flex lg:flex-row flex-col px-[4.6%] gap-10">
-        <div className="basis-1/2">
-          <Feed />
-        </div>
-        <div className="basis-1/2">
-          <Faq />
-        </div>
-      </div> */}
     </Layout>
   );
 };
