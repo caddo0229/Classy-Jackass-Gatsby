906fc155236888ccfd8590038a4c6c9a59e32e1e
unknown <hernandezcesar1028@gmail.com>
2023-01-18 12:31:40 -0500

    fix the error

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "6zrd6a5m",
-    dataset: process.env.GATSBY_SANITY_DATASET || "production",
+    projectId: process.env.SANITY_PROJECT_ID || "6zrd6a5m",
+    dataset: process.env.SANITY_PROJECT_DATASET || "production",
   },
 };
